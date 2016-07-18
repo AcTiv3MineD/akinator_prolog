b326a0da3af550cb0f621242de66056bb8c10667
AcMineD <acmined@gmail.com>
2016-07-18 00:54:06 -0400

    Diccionario de llaves, acciones servidor web, reimplementacion de reglas

--- js/general.js
+++ js/general.js
@@ -1,3 +1,62 @@
 $( document ).ready( function( ) {
-	//
+	$( "#iniciar_partida" ).click( function( ){
+		$.ajax( {
+			type: "post",
+			url: 'http://localhost:8002/iniciar',
+			data: {
+				
+			}
+		} ).done( function( data, textStatus, jqXHR ) {
+			$( ".contenedor_pregunta" ).fadeIn();
+			$( ".inicio" ).hide();
+			$( ".no_encontrado" ).hide();
+
+			$( "#pregunta_actual" ).val( data[ 1 ] );
+			$( "#persona_actual" ).val( data[ 0 ] );
+
+			procesar_pregunta( );
+		} );
+	} );
+
+	$( ".respuesta" ).click( function( ){
+		var respuesta = $( this ).attr( 'rel' );
+
+		$.ajax( {
+			type: "post",
+			url: 'http://localhost:8002/responder',
+			data: {
+				'respuesta' : respuesta,
+				'pregunta'  : $( "#pregunta_actual" ).val( )
+			}
+		} ).done( function( data, textStatus, jqXHR ) {
+			console.log( data );
+			if( data[ 2 ] == 'encontrado' ){
+				$( ".contenedor_pregunta" ).hide( );
+				$( ".resultado" ).text( dictionary[ data[ 0 ] ] );
+				$( ".final" ).fadeIn();
+				$( ".inicio" ).fadeIn();
+				return;
+			}
+
+			if( data[ 2 ] == 'desconocido' ){
+				$( ".no_encontrado" ).fadeIn();
+				$( ".contenedor_pregunta" ).hide( );
+				$( ".inicio" ).fadeIn();
+
+				return;
+			}
+
+			$( "#pregunta_actual" ).val( data[ 1 ] );
+			$( "#persona_actual" ).val( data[ 0 ] );
+
+			procesar_pregunta( );
+		} );
+	} );
+
+	function procesar_pregunta( ){
+		$( '#pregunta' ).typed( {
+			strings: [ dictionary[ $( "#pregunta_actual" ).val( ) ] ],
+			typeSpeed: 0
+		} );
+	}
 } );
--- main.pl
+++ main.pl
@@ -4,61 +4,425 @@
 	no/1.
 
 %% HIPOTESIS %%
-hipotesis( Persona, Respuesta ) :- persona( Persona, Respuesta ), write( 'Persona -> ' ), write( Persona ), nl, Respuesta == 'no', !, fail.
-hipotesis( Persona, Respuesta ) :- persona( Persona, Respuesta ), Respuesta == 'en_proceso', !.
-hipotesis( Persona, Respuesta ) :- persona( Persona, Respuesta ), Respuesta == 'encontrado', !.
-hipotesis( desconocido, desconocido ).
-
-%%hipotesis( juan_daniel_joa, Respuesta ) :- juan_daniel_joa( Respuesta ), Respuesta == 'en_proceso', !.
-%%hipotesis( nelson_daniel_duran, Respuesta ) :- nelson_daniel_duran, Respuesta == 'en_proceso', !.
-%%hipotesis( desconocido, desconocido ).
+hipotesis( Persona, Pregunta, Respuesta ) :- persona( Persona, Pregunta, Respuesta ), Respuesta == 'no', !, fail.
+hipotesis( Persona, Pregunta, Respuesta ) :- persona( Persona, Pregunta, Respuesta ), Respuesta == 'en_proceso', !.
+hipotesis( Persona, Pregunta, Respuesta ) :- persona( Persona, Pregunta, Respuesta ), Respuesta == 'encontrado', !.
+hipotesis( _, _, desconocido ).
 
 %% LISTAS %%
-lista_persona( juan_daniel_joa, [ 'es_sereno', 'pelo_color_negro', 'tiene_barba', 'estatura_promedio', 'peso_promedio', 'color_piel_blanca' ] ).
-lista_persona( nelson_daniel_duran, [ 'es_pasivo', 'pelo_color_negro', 'peso_flaco', 'estatura_alto', 'color_piel_mulato' ] ).
+lista_persona(
+juan_daniel_joa,
+[ 'es_sereno',
+'pelo_color_negro',
+'tiene_barba',
+'tiene_lente',
+'estatura_promedio',
+'peso_promedio',
+'es_narizu',
+'color_piel_blanca',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_negro'] ).
+
+
+lista_persona(
+nelson_daniel_duran,
+[ 'es_pasivo',
+'pelo_color_negro',
+'estatura_promedio',
+'peso_flaco',
+'color_piel_blanca',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_marron_oscuro'] ).
+
+lista_persona(
+jean_louis,
+[ 'es_pasivo',
+'pelo_color_negro',
+'tiene_barba',
+'tiene_lente',
+'estatura_pequeno',
+'peso_flaco',
+'color_piel_moreno',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_marron_oscuro'] ).
+
+lista_persona(
+pierre,
+[ 'es_pasivo',
+'pelo_color_negro',
+'tiene_barba',
+'tiene_lente',
+'estatura_alto',
+'peso_flaco',
+'color_piel_negro',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_marron_oscuro'] ).
+
+lista_persona(
+ansel_corona,
+[ 'es_sociable',
+'pelo_color_rubio',
+'tiene_barba',
+'estatura_alto',
+'peso_gordo',
+'es_narizu',
+'color_piel_blanca',
+'sexo_masculino',
+'color_ojos_verde'] ).
+
+
+lista_persona(
+eva_concepcion,
+[ 'es_sociable',
+'pelo_color_castano_oscuro',
+'estatura_promedio',
+'peso_promedio',
+'es_narizu',
+'color_piel_moreno',
+'es_aplicado',
+'sexo_femenino',
+'color_ojos_marron_oscuro'] ).
+
+
+lista_persona(
+john_jerry_bien_aime,
+[ 'es_agradable',
+'pelo_color_negro',
+'tiene_barba',
+'estatura_alto',
+'peso_promedio',
+'es_narizu',
+'color_piel_negro',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_negro'] ).
+
+
+lista_persona(
+gleyder_hernandez,
+[ 'es_sereno',
+'pelo_color_negro',
+'tiene_barba',
+'tiene_lente',
+'estatura_promedio',
+'peso_promedio',
+'es_narizu',
+'color_piel_moreno',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_marron_oscuro'] ).
+
+
+lista_persona(
+angel_gonzalez,
+[ 'es_sociable',
+'pelo_color_negro',
+'tiene_barba',
+'estatura_alto',
+'peso_promedio',
+'es_narizu',
+'color_piel_blanca',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_negro'] ).
+
+
+lista_persona(
+oasting_ferrera,
+[ 'es_sociable',
+'pelo_color_negro',
+'tiene_barba',
+'estatura_promedio',
+'peso_promedio',
+'color_piel_moreno',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_marron_oscuro'] ).
+
+
+lista_persona(
+francis_caceres,
+[ 'es_sociable',
+'pelo_color_negro',
+'tiene_barba',
+'tiene_lente',
+'estatura_pequeno',
+'peso_flaco',
+'color_piel_moreno',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_marron'] ).
+
+
+lista_persona(
+jesus_henriquez,
+[ 'es_sociable',
+'pelo_color_negro',
+'tiene_barba',
+'estatura_promedio',
+'peso_gordo',
+'color_piel_blanca',
+'sexo_masculino',
+'color_ojos_marron'] ).
+
+
+lista_persona(
+gustavo_henriquez,
+[ 'es_pasivo',
+'pelo_color_negro',
+'tiene_barba',
+'estatura_promedio',
+'peso_promedio',
+'es_narizu',
+'color_piel_blanca',
+'sexo_masculino',
+'color_ojos_marron_oscuro'] ).
+
+
+
+lista_persona(
+martin_martinez,
+[ 'es_sereno',
+'pelo_color_negro',
+'tiene_barba',
+'tiene_lente',
+'estatura_pequeno',
+'peso_gordo',
+'es_narizu',
+'color_piel_blanca',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_marron_oscuro'] ).
+
+
+lista_persona(
+isaac_perez,
+[ 'es_pasivo',
+'pelo_color_negro',
+'tiene_barba',
+'estatura_alto',
+'peso_gordo',
+'es_narizu',
+'color_piel_moreno',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_marron_oscuro'] ).
+
+
+lista_persona(
+dewyn_liriano,
+[ 'es_sociable',
+'pelo_color_castano',
+'tiene_barba',
+'estatura_alto',
+'peso_gordo',
+'color_piel_blanca',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_marron_oscuro'] ).
+
+
+
+lista_persona(
+stanley_de_moya,
+[ 'es_sociable',
+'pelo_color_castano',
+'estatura_promedio',
+'peso_promedio',
+'es_narizu',
+'color_piel_blanca',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_marron'] ).
+
+
+
+lista_persona(
+jose_miguel_marcano,
+[ 'es_sociable',
+'pelo_color_castano',
+'tiene_lente',
+'estatura_promedio',
+'peso_gordo',
+'es_narizu',
+'color_piel_blanca',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_marron'] ).
+
+
+lista_persona(
+cesar_mendez,
+[ 'es_sociable',
+'pelo_color_negro',
+'estatura_alto',
+'peso_flaco',
+'color_piel_moreno',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_marron'] ).
+
+
+lista_persona(
+carlos_batista,
+[ 'es_sociable',
+'pelo_color_castano',
+'estatura_promedio',
+'peso_promedio',
+'es_narizu',
+'color_piel_blanca',
+'es_aplicado',
+'sexo_masculino',
+'color_ojos_marron'] ).
+
+categoria( color_pelo, pelo_color_negro ).
+categoria( color_pelo, pelo_color_rubio ).
+categoria( color_pelo, pelo_color_castano_oscuro ).
+categoria( color_pelo, pelo_color_castano ).
+
+categoria( barba, tiene_barba ).
+categoria( lente, tiene_lente ).
+
+categoria( estatura, estatura_promedio ).
+categoria( estatura, estatura_pequeno ).
+categoria( estatura, estatura_alto ).
+
+categoria( peso, peso_promedio ).
+categoria( peso, peso_flaco ).
+categoria( peso, peso_gordo ).
+
+categoria( nariz, es_narizu ).
+categoria( color_piel, color_piel_blanca ).
+categoria( color_piel, color_piel_moreno ).
+categoria( color_piel, color_piel_negro ).
+
+categoria( sexo, sexo_masculino ).
+categoria( sexo, sexo_femenino ).
+
+categoria( color_ojos, color_ojos_marron ).
+categoria( color_ojos, color_ojos_negro ).
+categoria( color_ojos, color_ojos_verde ).
+categoria( color_ojos, color_ojos_marron_oscuro ).
+
+categoria( personalidad, es_sereno).
+categoria( personalidad, es_sociable ).
+categoria( personalidad, es_pasivo ).
+categoria( personalidad, es_agradable ).
+categoria( responsabilidad, es_aplicado ).
 
 %% PERSONAS %%
-persona( Persona, Estado ) :-
-	verificar_lista( Persona, Estado ).
+persona( Persona, Pregunta, Estado ) :-
+	verificar_lista( Persona, Pregunta, Estado ).
 
-verificar_lista( Persona, Estado ) :-
+verificar_lista( Persona, Pregunta, Estado ) :-
 	lista_persona( Persona, Lista ),
-	recorrer_lista( Lista, Estado ).
+	recorrer_lista( Lista, Pregunta, Estado ).
 
-recorrer_lista( [ ],  'encontrado' ).
-recorrer_lista( [ Cabeza | Cola ], Estado ) :-
+recorrer_lista( [ ], Pregunta, 'encontrado' ).
+recorrer_lista( [ Cabeza | Cola ], Pregunta, Estado ) :-
 	verifica( Cabeza, Resultado ),
 	(
-		Resultado == 'no' -> ( Estado = 'No', fail );
+		Resultado == 'no' -> ( Estado = 'No', Pregunta = Cabeza, fail );
 		(
-			Resultado == 'en_proceso' -> ( Estado = 'en_proceso', true );
-			recorrer_lista( Cola, Resp )
+			Resultado == 'en_proceso' -> ( Estado = 'en_proceso', Pregunta = Cabeza, true );
+			recorrer_lista( Cola, Temp, Resp ),
+			Pregunta = Temp
 		)
 	),
 	Estado = Resp.
 
-/*
-persona( nelson_daniel_duran, Estado ) :-
-	verifica( 'es_pasivo', Estado ),
-	verifica( 'pelo_color_negro', Estado ),
-	verifica( 'peso_flaco', Estado ),
-	verifica( 'estatura_alto', Estado ),
-	verifica( 'color_piel_mulato', Estado ),
-	verifica( 'es_aplicado', Estado ),
-	verifica( 'sexo_masculino', Estado ),
-	verifica( 'ojos_color_marron_oscuro', Estado ).
+procesar_respuesta( Respuesta, Pregunta ) :-
+	(
+		Respuesta == 'no' -> negar( Pregunta );
+		validar( Pregunta )
+	).
 
-*/
 %% HEHE
 verifica( Pregunta, Respuesta ) :- no( Pregunta ), Respuesta = 'no', !.
 verifica( Pregunta, Respuesta ) :- si( Pregunta ), Respuesta = 'encontrado', !.
-verifica( Pregunta, Respuesta ) :- preguntar( Pregunta ), Respuesta = 'en_proceso'.
+verifica( _, Respuesta ) :- Respuesta = 'en_proceso'.
 
-preguntar( X ) :- write( X ), nl,!.
-validar( X ) :- assert( si( X ) ).
+validar( X ) :- assert( si( X ) ), tachar_resto( X ).
 negar( X ) :- assert( no( X ) ).
+
+tachar_resto( X ) :-
+	categoria( Categoria, X ),
+	findall( Intermedio, categoria( Categoria, Intermedio ), Resultado ),
+	tachar_diferente( Resultado, X ).
+
+tachar_diferente( [ ], _ ).
+tachar_diferente( [ Cabeza | Cola ], X ) :-
+	Cabeza \= X,
+	negar( Cabeza ),
+	tachar_diferente( Cola, X ), !.
+
+tachar_diferente( [ Cabeza | Cola ], X ) :-
+	tachar_diferente( Cola, X ).
+
 
 %% LIMPIAR TODO
 limpiar :- retract( si( _ ) ), fail.
 limpiar :- retract( no( _ ) ), fail.
 limpiar.
+
+%%SERVIDOR WEB:
+%%LIBRERIAS
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_client)). %% NECESARIO PARA EL HTTP_READ_DATA
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_error)).
+:- use_module(library(http/html_write)).
+:- use_module(library(http/http_json)). %%Manejo JSON
+:- use_module(library(http/http_cors)). %%Manejo Cross-Origin Resource Sharing
+:- use_module(library(settings)).
+:- use_module(library(http/http_header)).
+
+%%CONFIGURACION INICIAL
+server(Port) :-
+		http_server( http_dispatch, [ port( Port ) ] ).
+
+:- set_setting( http:cors, [*] ).
+
+%%%%%%%%%%%%%%%%% RUTAS %%%%%%%%%%%%%%%%%%%%%%%%%
+%%RUTA PRINCIPAL
+:- http_handler( '/', say_hi, [ ] ).
+
+say_hi( _Request ) :-
+		format('Content-type: text/plain~n~n'),
+		cors_enable,
+		format('Hallo Leute!~n').
+
+%%RESETEAR
+:- http_handler( '/resetear', resetear_action, [] ).
+
+resetear_action( _Request ) :-
+		limpiar,
+		cors_enable,
+		format( 'sistema_limpiado' ).
+
+%%INICIAR PARTIDA
+:- http_handler( '/iniciar', iniciar_action, [] ).
+
+iniciar_action( _Request ) :-
+		member( method( post ), _Request ), !,
+		limpiar,
+		cors_enable,
+		hipotesis( Persona, Pregunta, Estado ),
+		reply_json_dict( [ Persona, Pregunta, Estado ] ).
+
+
+%%DAR RESPUESTA
+:- http_handler( '/responder', responder_action, [] ).
+
+responder_action( _Request ) :-
+		member( method( post ), _Request ), !,
+		http_read_data( _Request, [ respuesta = Respuesta, pregunta = Pregunta ], [ ] ),
+		procesar_respuesta( Respuesta, Pregunta ),
+		cors_enable,
+		hipotesis( Persona, NuevaPregunta, Estado ),
+		reply_json_dict( [ Persona, NuevaPregunta, Estado ] ).
